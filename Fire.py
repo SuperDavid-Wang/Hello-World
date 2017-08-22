ef0a969338de87dc26d739fae1fef83aa3dbf39c
SuperDave-Wang <31239008+SuperDave-Wang@users.noreply.github.com>
2017-08-22 16:49:21 +0800

    Update Fire.py

--- Fire.py
+++ Fire.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 ##字符串编码为utf-8格式，可以处理中文字符
 
-import urllib,urllib2  ##读取网页数据
-import json            ##在字符串数据和字典数据间进行转换
-import base64          ##将非ASCII数据转换为ASCII数据
-import sys             ##系统模块，用于程序和python解释器的交互
-import os              ##系统模块，用于程序和操作系统的交互
-import serial          ##串口通信
-import string          ##字符串操作
-import binascii        ##二进制数据和ASCII数据间的转换
 import time            ##操作时间
-import pygame
-import random
-import atexit
 import RPi.GPIO as gpio
 
 def init():
